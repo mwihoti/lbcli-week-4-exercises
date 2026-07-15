22f3069691c1b6b4ea3c7d93c882eb72c4d46a25
mwihoti <danielmwihoti@gmail.com>
2026-07-15 15:03:53 +0300

    fix 02: spend both UTXOs with locktime 2041

--- submission/02.sh
+++ submission/02.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
 # Create a raw transaction that can be spent in 2 weeks time, assuming the current block is 25
 
 # Amount of 20,000,000 satoshis to this address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP
 # Use the UTXOs from the transaction below
 transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+
+# Decode to get this transaction's txid (the UTXOs we spend are its outputs)
 decoded=$(bitcoin-cli -regtest decoderawtransaction "$transaction")
 txid=$(echo "$decoded" | jq -r '.txid')
 
-printf '%s' "$(bitcoin-cli -regtest -named createrawtransaction \
-  inputs='''[ { "txid": "'"$txid"'", "vout": 0, "sequence": 4294967294 }  ]''' \
+# 2 weeks ≈ 2016 blocks; current block 25 → locktime 25 + 2016 = 2041
+# Spend both outputs of the given transaction
+bitcoin-cli -regtest -named createrawtransaction \
+  inputs='''[ { "txid": "'"$txid"'", "vout": 0 }, { "txid": "'"$txid"'", "vout": 1 } ]''' \
   outputs='''{ "2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP": 0.2 }''' \
-  locktime=2041)"
+  locktime=2041
